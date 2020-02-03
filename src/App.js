2a50dc54baa33015351a3ba59f1dd5cdadd49167
HK <user@HK.local>
2020-02-03 19:48:01 +0900

    add movieInfo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import React from "react";
-import Axios from "axios";
-
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovie = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovie();
+  }
   render() {
-    return <></>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                rating={movie.rating}
+                summary={movie.summary}
+                genres={movie.genres.join(", ")}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+            )
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+function Movie({ year, rating, title, summary, genres, poster }) {
+  return (
+    <div className="movie">
+      <h1 className="movie__title">{title}</h1>
+      <img className="movie__poster" src={poster} alt={title} title={title} />
+      <h2 className="movie__year">Year : {year}</h2>
+      <h2 className="movie__rating">Rating : {rating} / 10</h2>
+      <h2 className="movie__genres">Genres : {genres} </h2>
+      <p className="movie_summarty"> {summary}</p>
+      <hr></hr>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  year: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+
+export default Movie;
